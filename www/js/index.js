a4f9623caf6af4d84b78214778c70a980e30c271
Abraham Wolfe <abraham.wolfe@cmcss.net>
2017-11-14 23:31:31 -0600

    wolfe change5

--- www/js/index.js
+++ www/js/index.js
@@ -23,11 +23,11 @@ var options = {
     getAccessToken: getForgeToken
 }
 
-//one.stl
-//var documentId = 'urn:dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6c3RlbWZvb3RiYWxsOXZueWRkcWphaWFiYWdzbmx0ZGxydzBmcnFja3B1a3Uvb25lLnN0bA';
+one.stl
+var documentId = 'urn:dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6c3RlbWZvb3RiYWxsOXZueWRkcWphaWFiYWdzbmx0ZGxydzBmcnFja3B1a3Uvb25lLnN0bA';
 
-two.stl
-var documentId = 'urn:dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6c3RlbWZvb3RiYWxsOXZueWRkcWphaWFiYWdzbmx0ZGxydzBmcnFja3B1a3UvdHdvLnN0bA';
+//two.stl
+//var documentId = 'urn:dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6c3RlbWZvb3RiYWxsOXZueWRkcWphaWFiYWdzbmx0ZGxydzBmcnFja3B1a3UvdHdvLnN0bA';
 
 //three.stl
 //var documentId = 'urn:dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6c3RlbWZvb3RiYWxsOXZueWRkcWphaWFiYWdzbmx0ZGxydzBmcnFja3B1a3UvdGhyZWUuc3Rs';
